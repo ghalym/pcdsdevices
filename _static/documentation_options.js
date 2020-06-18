6812db4e708531d5c7c38b01d593fb821e84be28
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-06-18 17:34:37 +0000

    Update docs after building Travis build 1011 of pcdshub/pcdsdevices
    The docs were built from the branch 'master' against the commit
    14b651fbea357f7f1827dea0bf46bed9804be0e3.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/pcdshub/pcdsdevices/jobs/351040089.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/test-environment/bin/doctr deploy . --built-docs docs/build/html --deploy-branch-name gh-pages

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.6.0+18.g45ca04c',
+    VERSION: '2.6.0+20.g14b651f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
